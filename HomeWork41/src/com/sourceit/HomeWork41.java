da1bb2a6a26735d040c01c5a402c9cf87d9aab29
Oleg <solegstar@gmail.com>
2018-08-09 22:04:18 +0300

    first commit HomeWork41

---
+++ HomeWork41/src/com/sourceit/HomeWork41.java
@@ -0,0 +1,13 @@
+package com.sourceit;
+
+public class HomeWork41 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println ( String.format ( "%.1f%n", Math.PI) + String.format ( "%.2f%n", Math.PI)
+							+ String.format ( "%.3f%n", Math.PI) + String.format ( "%.4f%n", Math.PI)
+							+ String.format ( "%.5f%n", Math.PI));	// Форматирование числа Пи
+
+	}
+}
